a5d80d726843020dacda355cac5442c7fadeafe4
zengchao <zengc.xu@outlook.com>
2024-04-08 13:31:00 +0800

    update

--- data.jl
+++ data.jl
@@ -20,40 +20,71 @@ struct researcher
     url::String
 end
 
-xuzengchao = researcher(
-    (first="Zengchao", last="Xu"),
-    "xuzengchao@shnu.edu.cn",
-    "Shanghai Normal University", 
-    "Department of Mathematics", # Lab for Educational Big Data and Policymaking
-    "100 Guilin Road, Xuhui District, Shanghai",
-    "xu-zc.site"
-)
+researchers = [
+    researcher(
+        (first="Zengchao", last="Xu"),
+        "xuzengchao@shnu.edu.cn",
+        "Shanghai Normal University", 
+        "Department of Mathematics", # Lab for Educational Big Data and Policymaking
+        "100 Guilin Road, Xuhui District, Shanghai",
+        "xu-zc.site"
+    ),
+    researcher(
+        (first="Shan", last="Luo"), 
+        "sluomath@sjtu.edu.cn",
+        "Shanghai Jiao Tong University", 
+        "School of Mathematical Sciences",
+        "800 Donghuan Road, Minhang District, Shanghai",
+        "https://math.sjtu.edu.cn/Default/teachershow/tags/MDAwMDAwMDAwMLKIetw"
+    ),
+    researcher(
+        (first="Zehua", last="Chen"),
+        "stachenz@nus.edu.sg",
+        "National University of Singapore",
+        "Department of Statistics and Applied Probability",
+        "",
+        "https://www.stat.nus.edu.sg/chen-zehua/",
+    ),
+    researcher(
+        (first="Yi", last="Zhang"),
+        "zhangyi@lixin.edu.cn",
+        "Shanghai Lixin University of Accounting and Finance",
+        "School of Insurance",
+        "995 Shangchuan Road, Pudong New Area, Shanghai",
+        ""
+    ),
+]
 
-luoshan = researcher(
-    (first="Shan", last="Luo"), 
-    "sluomath@sjtu.edu.cn",
-    "Shanghai Jiao Tong University", 
-    "School of Mathematical Sciences",
-    "800 Donghuan Road, Minhang District, Shanghai",
-    "https://math.sjtu.edu.cn/Default/teachershow/tags/MDAwMDAwMDAwMLKIetw"
-)
-
-chenzehua = researcher(
-    (first="Zehua", last="Chen"),
-    "stachenz@nus.edu.sg",
-    "National University of Singapore",
-    "Department of Statistics and Applied Probability",
-    "",
-    "https://www.stat.nus.edu.sg/chen-zehua/",
-)
-
-zhangyi = researcher(
-    (first="Yi", last="Zhang"),
-    "zhangyi@lixin.edu.cn",
-    "Shanghai Lixin University of Accounting and Finance",
-    "School of Insurance",
-    "995 Shangchuan Road, Pudong New Area, Shanghai",
-    ""
-)
 
 
+
+struct student
+    year::String,
+    major::String
+    level::String
+    name::String
+end
+
+students = [
+    student("2023-09", "应用统计", "专硕", "倪苑溶一"), 
+    student("2023-09", "应用统计", "专硕", "杨钰婷"), 
+    student("2023-09", "应用统计", "专硕", "肖梨"), 
+]
+
+
+
+
+
+struct teaching
+    year::String
+    semester::String
+    level::String
+    course::String
+end
+
+courses = [
+    teaching("2024", "春", "本科生", "统计计算"),
+    teaching("2024", "春", "本科生", "概率论与数理统计"),
+    teaching("2023", "秋", "研究生", "应用数理统计"),
+    teaching("2023", "春", "本科生", "概率论与数理统计"),
+]
--- index.md
+++ index.md
@@ -7,7 +7,6 @@
 \icon{envelope-fill} Email: `xuzengchao@domain` with `domain=shnu.edu.cn` <!-- `Y=sjtu.edu.cn` 20220901-->
 
 <!-- \icon{geo-alt-fill} Minhang campus, Shanghai Jiao Tong University, 200240, Shanghai. -->
-<!-- \icon{geo-alt-fill} Office 110 in math building 3, Xuhui campus, Shanghai Normal Univ., 100 Guilin RD Shanghai. -->
 \icon{geo-alt-fill} Office 110 in math building, Xuhui campus, Shanghai Normal Univ., 100 Guilin RD, Shanghai.
 
 
